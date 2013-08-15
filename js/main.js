053a55f72d3547763f77bed24cabcbf05f35a1e9
danielbjoseph <djosep@gmail.com>
2013-08-14 21:50:07 -0400

    Adding geospatial section images

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ $(document).ready(function(){
   		interval: 6000
 	});
 });
+
+function callModal (item) {
+	var title = $(item).children('.caption').html();
+	$(".modal-title").empty();
+	$(".modal-title").append(title);
+	
+	var thumbSrc = $(item).children('img').attr("src");
+	var imgSrc = thumbSrc.replace("_thumb", "");
+	var imgHtml = '<img src="' + imgSrc + '" alt="" class="img-responsive">'
+	$(".modal-body").empty();
+	$(".modal-body").append(imgHtml);
+	$('#myModal').modal();
+}
